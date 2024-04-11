3e5b8cf2e1261900b5dcb8b6a10c1ff660ae245a
vivektangudu123 <vivektangudu2383@gmail.com>
2024-04-11 11:18:24 +0530

    change

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 echo -e 'Starting importing of data'
-# python manage.py import_data
+python manage.py import_data
 echo -e 'Importing of data is complete'
